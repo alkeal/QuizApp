2b468cda3fcff95d76757092b9e5468f244846fe
alkeal <yhma22.alkeal@folkuniversitetet.nu>
2023-09-12 16:28:40 +0200

    Frågor finns nu

--- quiz.js
+++ quiz.js
@@ -13,4 +13,47 @@ button.addEventListener('click', async e => {
  const quizData = await response.json();
  console.log('3. got data:', quizData);
 
+
+  const questions = quizData.results;
+  createQuestions(questions);
+
 })
+
+
+const createQuestions = (questions) => {
+
+    const questionContainer = document.querySelector('#questions');
+
+    questions.forEach(question => {
+        
+    const questionElement = createQuestionElement(question);
+      questionContainer.appendChild(questionElement);
+    });
+
+
+
+
+
+
+
+
+}
+
+const createQuestionElement = (question) => {
+
+  const questionElement = document.createElement('div');
+  questionElement.className = 'question';
+
+  const questionHeading = document.createElement('h2');
+  questionHeading.innerHTML = question.question;
+
+  questionElement.appendChild(questionHeading);
+
+ 
+
+  return questionElement;
+
+
+
+
+}
